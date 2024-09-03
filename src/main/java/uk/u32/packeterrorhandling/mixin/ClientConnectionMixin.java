2ef39fc57cc2264295cc8892f4d73f26f01e280a
u32 <u32@u32.uk>
2024-09-03 07:17:06 +0100

    remove debug

--- src/main/java/uk/u32/packeterrorhandling/mixin/ClientConnectionMixin.java
+++ src/main/java/uk/u32/packeterrorhandling/mixin/ClientConnectionMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Debug(export = true)
 @Mixin(ClientConnection.class)
 public abstract class ClientConnectionMixin extends SimpleChannelInboundHandler<Packet<?>> {
     @Final
